ae51ca155e4ce6c6d1fcb2b8ba8ef53fd570a4bf
Lim Yu Yang <87360806+gnay-uy@users.noreply.github.com>
2024-04-28 22:49:03 +0800

    first commit

---
+++ app/src/main/java/sg/edu/np/mad/madpractical3/User.java
@@ -0,0 +1,21 @@
+package sg.edu.np.mad.madpractical3;
+public class User {
+    String name;
+    String description;
+    int id;
+    boolean followed;
+
+    public User() {
+        name = "default";
+        description = "default";
+        id = 0;
+        followed = false;
+    }
+
+    public User(String n, String d, int i, boolean f) {
+        name = n;
+        description = d;
+        id = i;
+        followed = f;
+    }
+}
